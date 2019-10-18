554d6c7bca20299d03aa181c595b0335dcd4467f
hr1d1 <53426170+hr1d1@users.noreply.github.com>
2019-10-18 07:22:17 +0600

    Update joincourse.js

--- js/joincourse.js
+++ js/joincourse.js
@@ -3,11 +3,22 @@ const join_form = document.querySelector('#joinForm');
 join_form.addEventListener('submit',(e) =>{
   e.preventDefault();
 
-  const ct="";
+  var ct="";
   const cc=join_form['coursecode'].value;
-  const chr="";
+  var chr="";
   const umail = firebase.auth().currentUser.email;
+  var j=false;
+  for (var i = 0; i < cList.length; i++) {
+    if(cList[i]==cc){
+      ct=tList[i];
+      chr=crList[i];
+    j=true;
+      break;
+    }
+  }
 
+  if(j == true)
+{
   db.collection('Courses').add({
     Code:cc,
     Title:ct,
@@ -21,21 +32,38 @@ join_form.addEventListener('submit',(e) =>{
   	alert("There seems to be a problem. Please try again.");
   })
 
-
+}
+else{
+  alert("Invalid Course Code.Please try again.");
+  join_form.reset();
+  //console.log(ct);
+}
 })
+
+
+var tList = new Array();
+var crList = new Array();
+var cList = new Array();
+
 db.collection('Courses').get().then(snapshot =>{
+  var i=0;
    snapshot.forEach(doc =>{
-     let cc = doc.data().Code;
+     cList[i] = doc.data().Code;
+     crList[i] = doc.data().CreditHr;
+     tList[i] = doc.data().Title;
+     i++;
    //  console.log(cc);
+    let cc = doc.data().Code;
      db.collection('Courses').where('Code' , '==', cc).get().then(snapshotDoc =>{
        snapshotDoc.forEach(sdoc =>{
          if(doc.data().Title == "" && sdoc.data().Title > ""){
            doc.data().Title = sdoc.data().Title;
            doc.data().CreditHr = sdoc.data().CreditHr;
          }
-         console.log(sdoc.data());
+        // clist[i] =  sdoc.data();
        })
-       console.log(doc.data());
+
+    //   console.log(doc.data());
 
 
      })
@@ -43,10 +71,11 @@ db.collection('Courses').get().then(snapshot =>{
    })
 
  })
-
-
-
- /*setTimeout(function(){
- document.location.href="#";
-},50000);
-})*/
+ function AddCourseForm(){
+   document.getElementById("addform").style.display = "block";
+   document.getElementById("courelist").style.display = "none";
+ }
+ function courseForm(){
+   document.getElementById("joinForm").style.display = "block";
+   document.getElementById("courelist").style.display = "none";
+ }
